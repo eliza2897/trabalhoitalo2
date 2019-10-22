be88f1a5eed3a14b32fb76e835e31682dc0a52d0
eliza <elizangeladasilva311@gmail.com>
2019-10-22 15:52:28 -0200

    Projeto para monitoria voluntaria

---
+++ index.php
@@ -0,0 +1,73 @@
+
+<!-- Elizangela-->
+
+<html>
+     
+
+<div style=" color: black; font-size: 30px;"> <p><b> Cadastro de Estudante Para Monitoria Voluntaria</p></b></div>
+<form method="" action="">
+<div style=" color: black;font-size: 20px;">
+Nome Completo:<input type="text"  name="nome"/><br>
+
+Sexo:
+<input type="radio" name="gender"value="M"checked> M  
+<input type="radio" name="gender"value="F"> F
+<input type="radio" name="gender"value="Outros"> Outros<br>
+Telefone:<input type="text" name="Telefone"/><br>
+Data de Nascimento:<input type="text" name="Nascimento"/> <br>
+Email:<input type="text" name="Email"/><br>
+Endereço:<input type="text" name="Endereço"/><br>
+Cidade:<input type="text" name="Cidade"/><br>
+Disciplina:<input type="text" name="Disciplina"/><br>
+Curso:<input type="text" name="Curso"/><br>
+Matricula:<input type="text" name="Matricula"/><br>
+Estado:
+  <select name="Estado">
+    <option value="AC">Acre</option>
+    <option value="AL">Alagoas</option>
+    <option value="AP">Amapá</option>
+    <option value="AM">Amazonas</option>
+    <option value="BA">Bahia</option>
+    <option value="CE">Ceará</option>
+    <option value="DF">Distrito Federal</option>
+    <option value="ES">Espírito Santo</option>
+    <option value="GO">Goiás</option>
+    <option value="MA">Maranhão</option>
+    <option value="MT">Mato Grosso</option>
+    <option value="MS">Mato Grosso do Sul</option>
+    <option value="MG">Minas Gerais</option>
+    <option value="PA">Pará</option>
+    <option value="PB">Paraíba</option>
+    <option value="PR">Paraná</option>
+    <option value="PE">Pernambuco</option>
+    <option value="PI">Piauí</option>
+    <option value="RJ">Rio de Janeiro</option>
+    <option value="RN">Rio Grande do Norte</option>
+    <option value="RS">Rio Grande do Sul</option>
+    <option value="RO">Rondônia</option>
+    <option value="RR">Roraima</option>
+    <option value="SC">Santa Catarina</option>
+    <option value="SP">São Paulo</option>
+    <option value="SE">Sergipe</option>
+    <option value="TO">Tocantins</option><br>
+  </select>
+  <br><br>
+Bairro: <input type="text" name="Bairro"/><br>
+ Etnia:
+<select name="Etnia"><br><br>
+    <option value="Branca">Branca</option><br>
+   <option value="Parda">Parda</option>
+   
+    <option value="Preta">Preta</option>
+    <br>
+</select><br>
+<br>
+ <input type="submit" value="CADASTRAR" ><br><br>
+</div>
+
+</form>
+</body>
+</head>
+</style>
+</html>
+<!-- Elizangela-->
