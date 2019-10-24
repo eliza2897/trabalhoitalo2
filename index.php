e92799c6ccea3830de24a92f8d72026b011ee123
eliza2897 <54370076+eliza2897@users.noreply.github.com>
2019-10-24 14:34:12 -0300

    alteração no código

--- index.php
+++ index.php
@@ -2,68 +2,31 @@
 <!-- Elizangela-->
 
 <html>
-     
+ <form>    
+<head>
+<style type="text/css"></style>
 
-<div style=" color: black; font-size: 30px;"> <p><b> Cadastro de Estudante Para Monitoria Voluntaria</p></b></div>
+<div style=" color: black; font-size: 30px;"> <p><b> Cadastro Para Monitoria Voluntaria</p></b></div>
 <form method="" action="">
 <div style=" color: black;font-size: 20px;">
-Nome Completo:<input type="text"  name="nome"/><br>
-
-Sexo:
-<input type="radio" name="gender"value="M"checked> M  
-<input type="radio" name="gender"value="F"> F
-<input type="radio" name="gender"value="Outros"> Outros<br>
-Telefone:<input type="text" name="Telefone"/><br>
-Data de Nascimento:<input type="text" name="Nascimento"/> <br>
-Email:<input type="text" name="Email"/><br>
-Endereço:<input type="text" name="Endereço"/><br>
-Cidade:<input type="text" name="Cidade"/><br>
-Disciplina:<input type="text" name="Disciplina"/><br>
-Curso:<input type="text" name="Curso"/><br>
+Aluno:<input type="text"  name="Aluno"/><br>
 Matricula:<input type="text" name="Matricula"/><br>
-Estado:
-  <select name="Estado">
-    <option value="AC">Acre</option>
-    <option value="AL">Alagoas</option>
-    <option value="AP">Amapá</option>
-    <option value="AM">Amazonas</option>
-    <option value="BA">Bahia</option>
-    <option value="CE">Ceará</option>
-    <option value="DF">Distrito Federal</option>
-    <option value="ES">Espírito Santo</option>
-    <option value="GO">Goiás</option>
-    <option value="MA">Maranhão</option>
-    <option value="MT">Mato Grosso</option>
-    <option value="MS">Mato Grosso do Sul</option>
-    <option value="MG">Minas Gerais</option>
-    <option value="PA">Pará</option>
-    <option value="PB">Paraíba</option>
-    <option value="PR">Paraná</option>
-    <option value="PE">Pernambuco</option>
-    <option value="PI">Piauí</option>
-    <option value="RJ">Rio de Janeiro</option>
-    <option value="RN">Rio Grande do Norte</option>
-    <option value="RS">Rio Grande do Sul</option>
-    <option value="RO">Rondônia</option>
-    <option value="RR">Roraima</option>
-    <option value="SC">Santa Catarina</option>
-    <option value="SP">São Paulo</option>
-    <option value="SE">Sergipe</option>
-    <option value="TO">Tocantins</option><br>
-  </select>
-  <br><br>
-Bairro: <input type="text" name="Bairro"/><br>
- Etnia:
-<select name="Etnia"><br><br>
-    <option value="Branca">Branca</option><br>
-   <option value="Parda">Parda</option>
-   
-    <option value="Preta">Preta</option>
-    <br>
-</select><br>
-<br>
- <input type="submit" value="CADASTRAR" ><br><br>
+Email:<input type="text" name="Email"/><br>
+
+Curso:<input type="text" name="Curso"/><br>
+
+Período que está matriculado:<input type="text" name="Período que está matriculado"/><br>
+Telefone:<input type="number" name="Telefone"/><br>
+
+Disciplina em que deseja ser monitor:<input type="text" name="Disciplina "/><br>
+Já foi monitor:<input type="text" name="Já foi monitor"/<br><br>
+
+
+ <input type="submit" value="GRAVAR" ><br><br>
 </div>
+<input type="submit" value="IMPRIMIR" ><br><br>
+</div>
+
 
 </form>
 </body>
